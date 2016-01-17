961d2d34770cc16cda6a00888e718a3d331667ed
Guttyon3 <y.yamaguchi0329@gmail.com>
2016-01-17 21:19:55 +0900

    Goalオブジェクト変更/アニメーション付与

---
+++ HyperBall/Assets/YY/Scripts/ObjectRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour {
+
+    private float _ObjRotation = 0;
+
+    void Update() {
+        _ObjRotation += Time.deltaTime;
+        transform.Rotate(0, 0, _ObjRotation);
+    }
+}
