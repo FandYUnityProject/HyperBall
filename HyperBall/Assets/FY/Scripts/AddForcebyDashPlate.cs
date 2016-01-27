306b065258642393f6c7fad2ce1527759dc817eb
whitewhale6 <whitewhale6@gmail.com>
2016-01-27 19:20:49 +0900

    Add "AddForcebyDashPlate.cs" and delete "HyperBall_TouchDashPlate_Obj.cs"

---
+++ HyperBall/Assets/FY/Scripts/AddForcebyDashPlate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForcebyDashPlate : MonoBehaviour {
+
+    Rigidbody Prb;
+    GameObject Player;
+    Vector3 PlateDir;
+    public float AddSpeed = 1000.0f;
+
+    void Start()
+    {
+        Prb = GameObject.Find("HyperBall").GetComponent<Rigidbody>();
+        PlateDir = GetComponent<Transform>().up;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        //DashPlate接触時に、HyperBallに推進力を与える
+        if (coll.gameObject.tag == "Player")
+        {
+            DebugInfo_Manager.DebugInfo_Update("DashPlateに接触しました");
+            Vector3 ToDirection = PlateDir * AddSpeed;
+            Prb.AddForce(ToDirection);
+            Debug.Log(ToDirection);
+        }
+    }
+}
